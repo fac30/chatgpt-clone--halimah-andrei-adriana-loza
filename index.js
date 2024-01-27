95b667d1607e6835cd89db03455a30ac5c42f4bc
AdrianaIaffa <adrianaiaffa@hotmail.com>
2024-01-27 12:19:52 +0000

    fetch/headers

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ form.addEventListener('submit', (event) => {
     const chatOutput = document.getElementById('chat-output');
 
     chatOutput.textContent = chatInput;
+
+    // const apiUrl = `https://api.openai.com/v1/chat/completions/`
+
+    fetch(`https://api.openai.com/v1/chat/completions/`, {
+        method: 'POST',
+        body: JSON.stringify(chatInput),
+        headers: {
+            'Content-Type' : 'application/json',
+            'authorization': `Bearer ${APIKEY}`,
+        }
+       
+
+    
+
+    })
 })
